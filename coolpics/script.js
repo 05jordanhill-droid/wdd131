5a6c2498db60faa62d4d25dcdb7ad7bef9a145d9
05jordanhill-droid <05jordanhill@gmail.com>
2026-05-17 00:36:54 -0600

    update

---
+++ coolpics/script.js
@@ -0,0 +1,47 @@
+const btn = document.querySelector(".menu");
+const menu = document.querySelector("nav")
+btn.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+    menu.classList.toggle("hide");
+    btn.classList.toggle("change");
+}
+
+
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    
+    const img = e.target;
+
+    const src = img.getAttribute("src");
+    const alt = img.getAttribute("alt");
+
+    const full = src.replace("sm", "full");
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+
+    console.log("before")
+
+    modal.showModal();
+    console.log("after")
+
+}
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
